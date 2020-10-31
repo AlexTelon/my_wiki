dd0f79a670f303e61aa201c469423d447505842d
Alex Telon <alex.telon@gmail.com>
2020-10-31 18:59:25 +0100

    Server now serves the output dir

--- http.py
+++ http.py
@@ -2,8 +2,14 @@ import http.server
 import socketserver
 
 PORT = 8000
+OUTPUT_DIR = 'output'
 
-Handler = http.server.SimpleHTTPRequestHandler
+# Handler = http.server.SimpleHTTPRequestHandler(directory=OUTPUT_DIR)
+
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=OUTPUT_DIR, **kwargs)
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
